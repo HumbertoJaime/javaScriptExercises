b3718cf98e6688f115a9ee0272ce2fb124aaa6c3
HumbertoJaime <humbertojaime@MacBook-Air-de-Humberto.local>
2024-09-15 12:49:41 -0600

    Ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,13 @@
+/*EJERCICIO 1 
+    Dado un número, devolver su tabala de múltiplicar completa*/
+
+function multiplicar (number)
+{
+    for(i=0; i<=10; i++)
+    {
+        console.log(number +"*"+ i +"="+ number*i)
+
+    }
+}
+
+multiplicar (5)
